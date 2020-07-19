c5c1f866538cc2a3acea8b5216c0552f88c8690d
anthonycede <anthony.cedeno.laporte@gmail.com>
2020-07-18 22:55:00 -0400

    Created results page

--- lib/imput_page.dart
+++ lib/imput_page.dart
@@ -4,6 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import './icon_and_text.dart';
 import './reusable_card.dart';
 import './constants.dart';
+import './results_page.dart';
 
 enum Gender {
   male,
@@ -210,11 +211,18 @@ class _ImputPageState extends State<ImputPage> {
               ],
             ),
           ),
-          Container(
-            color: kBottomContainerColor,
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ResultsPage()));
+            },
+            child: Container(
+              child: Text('CALCULATE'),
+              color: kBottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           )
         ],
       ),
